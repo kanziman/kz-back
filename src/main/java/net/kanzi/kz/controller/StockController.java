a7d734254161c96afb1417c2608a0f12f1b056b0
kanziman <kanzirunner@gmail.com>
2023-12-14 21:41:07 +0900

    cors

--- src/main/java/net/kanzi/kz/controller/StockController.java
+++ src/main/java/net/kanzi/kz/controller/StockController.java
@@ -8,10 +8,7 @@ import net.kanzi.kz.service.CorpService;
 import net.kanzi.kz.service.MarketService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -20,6 +17,7 @@ import java.util.Map;
 @RequiredArgsConstructor
 @RestController
 @Log4j2
+@CrossOrigin("*")
 public class StockController {
 
     private final CorpService corpService;
