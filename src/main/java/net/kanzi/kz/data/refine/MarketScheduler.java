a12581dbcc5c98bc0f9b58e80e2137efa04cb7d1
kanziman <kanzirunner@gmail.com>
2024-03-14 15:42:12 +0900

    scheduler off

--- src/main/java/net/kanzi/kz/data/refine/MarketScheduler.java
+++ src/main/java/net/kanzi/kz/data/refine/MarketScheduler.java
@@ -3,7 +3,6 @@ package net.kanzi.kz.data.refine;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import net.kanzi.kz.data.aop.Trace;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.sql.Date;
@@ -17,7 +16,7 @@ public class MarketScheduler {
 
     private final MarketRefineService marketRefineService;
 
-    @Scheduled(cron = "0 0 22 * * *")
+//    @Scheduled(cron = "0 0 22 * * *")
 //    @Scheduled(cron = "* */1 * * * *")
     @Trace
     public void scheduleTask() throws SQLException {
